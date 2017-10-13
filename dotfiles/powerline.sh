d6a6871a073a70fbaca7c4c77892dcdcd54bda2b
neze <neze@eleves.polytechnique.fr>
2017-10-13 20:25:24 +0200

    [powerline] no re-install
    Signed-off-by: neze <neze@eleves.polytechnique.fr>

---
+++ dotfiles/powerline.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+script_dir=$( cd "$( dirname "$0" )" && pwd)
+
+if [[ `uname` == 'Darwin' ]]; then
+  # MacOS
+  font_dir="$HOME/Library/Fonts"
+else
+  # Linux
+  font_dir="$HOME/.local/share/fonts"
+fi
+
+if [ ! -f "$font_dir/.powerline.ok" ]; then
+  source "$script_dir/powerline/install.sh" &&
+  touch "$font_dir/.powerline.ok"
+fi
